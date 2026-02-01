43ff60580ada095b5dcc9eaf990b7e4c60864568
Shaik Abdul Farooq <af453498@gmail.com>
2026-02-02 04:25:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -319,4 +319,6 @@ with app.app_context():
         print("Default admin created: admin@example.com / admin123")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT",8000))
+    app.run(host="0.0.0.0", port=port)
